e32da4f478e13d1e90ede81849f270767deb3858
azhan261 <azhan261@gmail.com>
2021-11-09 11:03:53 +0500

    New commit

---
+++ client/build/precache-manifest.33f0913bbe931f19c9055db168078aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f00df4c70925c561d22a4667bc3a7dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d98fc3187b730e70a276",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "401e745065819688c86e",
+    "url": "/static/js/2.e7b1de21.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.e7b1de21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d98fc3187b730e70a276",
+    "url": "/static/js/main.195486a8.chunk.js"
+  },
+  {
+    "revision": "4387b907a4d64303b5a2",
+    "url": "/static/js/runtime-main.fab42d54.js"
+  }
+]);
